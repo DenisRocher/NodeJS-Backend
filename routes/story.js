3f6b39b8db33ab69cc24c52c8cc27d1e89efa43f
Denis Rocher <denis.rocher@gmail.com>
2021-03-16 18:30:38 -0300

    Modelo para creación de relatos

--- routes/story.js
+++ routes/story.js
@@ -6,11 +6,9 @@ var router = express.Router();
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart({uploadDir: './uploads'});
 
-router.get('/home', StoryController.home);
-router.post('/test', StoryController.test);
 router.post('/save-story', StoryController.saveStory);
-router.post('/get-story/:id?', StoryController.getStory);
-router.post('/get-storieslist/:languages?', StoryController.getStories);
+router.get('/relato/:id?', StoryController.getStory);
+router.get('/relatos/:languages?', StoryController.getStories);
 router.put('/update-story/:id?', StoryController.updateStory);
 router.delete('/delete-story/:id?', StoryController.deleteStory);
 router.post('/upload-image/:id?', multipartMiddleware,StoryController.uploadImage);
